c09695a3ba578da3bc8f2fc815becda3e7df6aa8
zsheng <zsheng.dev@gmail.com>
2025-03-07 12:45:05 -0500

    chore: init wxt

---
+++ apps/extension/wxt.config.ts
@@ -0,0 +1,11 @@
+import tailwindcss from '@tailwindcss/vite';
+import { defineConfig } from 'wxt';
+
+// See https://wxt.dev/api/config.html
+export default defineConfig({
+  extensionApi: 'chrome',
+  modules: ['@wxt-dev/module-react'],
+  vite: () => ({
+    plugins: [tailwindcss()],
+  }),
+});
